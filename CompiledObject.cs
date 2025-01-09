f119035194812b054452e2ce523032562938d256
solarnomad7 <185842118+solarnomad7@users.noreply.github.com>
2025-01-09 17:01:25 +0200

    Revert "Initial commit"
    This reverts commit cf7795efcb3bd58d140aeaef99287ba9da0aef6f.

--- CompiledObject.cs
+++
@@ -1,9 +0,0 @@
-namespace MintCompiler
-{
-    public class CompiledObject(byte[] instructions, Dictionary<string, byte[]> labels, ushort numPointers)
-    {
-        public readonly byte[] Instructions = instructions;
-        public readonly Dictionary<string, byte[]> Labels = labels;
-        public readonly ushort NumPointers = numPointers;
-    }
-}
